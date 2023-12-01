8b9369bb689b7531be9208565eff38f0df4747d5
Swiatoslaw <swiatoslaw.p@gmail.com>
2023-12-01 21:12:46 +0100

    've been working on the destination_floors function, defined a structured approach, still doesn't quite get the right message

--- main.c
+++ main.c
@@ -21,6 +21,9 @@ int numFloors(char *hotel_name);
 int numElevators(char *totel_name);
 int elevCapacity(char *hotel_name);
 int people_w(void);
+int destination_floors(int floors, int people_waiting);
+
+
 
 int main(void)
 {
@@ -30,6 +33,7 @@ int main(void)
   int elevators = numElevators(p);//collect the number of elevators;
   int elev_cap = elevCapacity(p);//collect the elevator capacity;
   int people_waiting = people_w();//collect the amount of waiting people;
+  destination_floors(floors, people_waiting);
   printf("%s, %d, %d, %d, %d", p, floors, elevators, elev_cap, people_waiting);
 
   //freeing the memory
@@ -65,8 +69,7 @@ char *nameHotel(void)
     printf("Unable to allocate memory for a name of the hotel.");
   }
   printf("Enter the name of the hotel:\n >");
-  char *str_p = string;
-  fgets(str_p, sizeof(size_t)* 100, stdin);
+  fgets(string, sizeof(size_t)* 100, stdin);
   return string;
 }
 //---------------------------------------------------------------------------------------------------------------------
@@ -76,15 +79,13 @@ char *nameHotel(void)
 /// @return the number of floors
 int numFloors(char *hotel_name)
 {
-    //dynamic memory allocation:
-    // int *p = (int *)malloc(sizeof(int));
-    printf("Enter the number of floors in hotel %s:\n > ", hotel_name);
-    int digit  = 0;
+  //dynamic memory allocation:
+  // int *p = (int *)malloc(sizeof(int));
+  printf("Enter the number of floors in hotel %s:\n > ", hotel_name);    int digit  = 0;
+  scanf("%d", &digit);
+  while( (digit > 10 || digit < 3)){      printf("Wrong input, the number of floors must be between 3 and 10!\n");
     scanf("%d", &digit);
-    while( (digit > 10 || digit < 3)){
-      printf("Wrong input, the number of floors must be between 3 and 10!\n");
-      scanf("%d", &digit);
-    }
+  }
   return digit;
 }
 //---------------------------------------------------------------------------------------------------------------------
@@ -94,13 +95,13 @@ int numFloors(char *hotel_name)
 /// @return the number of elevators
 int numElevators(char *hotel_name)
 {
-    printf("Enter the number of elevators in hotel %s:\n > ", hotel_name);
-    int elv_num  = 0;
+  printf("Enter the number of elevators in hotel %s:\n > ", hotel_name);
+  int elv_num  = 0;
+  scanf("%d", &elv_num);
+  while( (elv_num < 1 || elv_num > 5)){
+    printf("Wrong input, the number of elevators must be between 1 and 5!\n");
     scanf("%d", &elv_num);
-    while( (elv_num < 1 || elv_num > 5)){
-      printf("Wrong input, the number of elevators must be between 1 and 5!\n");
-      scanf("%d", &elv_num);
-    }
+  }
   return elv_num;
 }
 
@@ -111,13 +112,13 @@ int numElevators(char *hotel_name)
 /// @return the capacity of each elevator
 int elevCapacity(char *hotel_name)
 {
-    printf("Enter the capacity of elevators in hotel %s:\n > ", hotel_name);
-    int elv_cap  = 0;
+  printf("Enter the capacity of elevators in hotel %s:\n > ", hotel_name);
+  int elv_cap  = 0;
+  scanf("%d", &elv_cap);
+  while( (elv_cap < 0 || elv_cap > 9)){
+    printf("Wrong input, the capacity of elevators must be between 1 and 9 person(s)!\n");
     scanf("%d", &elv_cap);
-    while( (elv_cap < 0 || elv_cap > 9)){
-      printf("Wrong input, the capacity of elevators must be between 1 and 9 person(s)!\n");
-      scanf("%d", &elv_cap);
-    }
+  }
   return elv_cap;
 }
 //---------------------------------------------------------------------------------------------------------------------
@@ -127,13 +128,14 @@ int elevCapacity(char *hotel_name)
 /// @return the number of waiting people
 int people_w(void)
 {
-    printf("Enter the number of people waiting on each floor:\n > ");
-    int p_w  = 0;
+  printf("Enter the number of people waiting on each floor:\n > ");
+  int p_w  = 0;
+  scanf("%d", &p_w);
+  while( (p_w < 2 || p_w > 20)){
+    printf("Wrong input, the number of people waiting on each floor must be between 2 and 20!\n");
     scanf("%d", &p_w);
-    while( (p_w < 2 || p_w > 20)){
-      printf("Wrong input, the number of people waiting on each floor must be between 2 and 20!\n");
-      scanf("%d", &p_w);
-    }
+  }
+  getchar();//get that enter
   return p_w;
 }
 //---------------------------------------------------------------------------------------------------------------------
@@ -141,18 +143,71 @@ int people_w(void)
 /// This function collects the amount of waiting people and returns the result in type int;
 /// @param void (no parameters)
 /// @return the number of waiting people
-int destination_floors(int floors)
+int destination_floors(int floors, int p_w)
 {
-    printf("Enter the number of people waiting on each floor:\n > ");
-    int p_w  = 0;
-    scanf("%d", &p_w);
-    while( (p_w < 2 || p_w > 20)){
-      printf("Wrong input, the number of people waiting on each floor must be between 2 and 20!\n");
-      scanf("%d", &p_w);
-    }
-  return p_w;
+  // char *pointer = (char  *)malloc(sizeof(char)*floors*p_w*2);//10 floors, each at max 10 characters(a little bit more)
+  // if(pointer == NULL){
+  //   printf("Could not allocate memory for the arrays of values.");
+  // }
+  //array of pointers:
+  //allocate:
+  char *arr = (char *)malloc(sizeof(char)*p_w*2);
+  if(arr == NULL){
+    printf("Could not allocate memory for string storage.");
+  }
+  // char *temp = (char *)malloc(sizeof(char)*p_w*2);
+  // for(int current_floor = 1; current_floor <= floors; current_floor++){
+    int current_floor = 1;
+    while(current_floor <= floors){//
+    //each value of the row must be less or equal to the number of the floors;
+    //the number of numbers will be from 2 to twenty(we must specify for each person):
+    const size_t size = sizeof(char) * floors * 2;
+    int done = 0;
+      while(!done){
+        done = 1;
+        printf("Enter the destination floors of the people [floor: %d]:\n > ", current_floor);
+        fgets(arr, size, stdin);
+        printf("%s", arr);
+        //assess the validity of our array:
+        char c = 0;
+        while((c != ',') && (c = *arr++) != '\0')//move through each character
+        {
+          if(c == '\n' || c == EOF){//skip the enters that are entered each time
+            continue;
+          }
+          if((c + '0') > (floors))//(c + '0') is converting c to a number
+          {
+            printf("Wrong input, the destination floor %c ", c);
+            printf("is out of range (0 to %d)!\n", floors - 1);
+            done = 0;
+          }else if((c + '0') == current_floor){
+            printf("Wrong input, the destination floor cannot be the current floor!\n");
+            done = 0;
+          }
+          
+          
+        }
+        // arr++;
+      }
+    ++current_floor;
+  }
+  //free the memory:
+  free(arr);
+  return 0;
 }
+//---------------------------------------------------------------------------------------------------------------------
+///
+/// This function parses the line:
+/// @param *arr pointer to the line of the input of the destination_floors function
+/// @param floors is the number of floors, type int
+/// @param current_floor is the current floor, type int
 
+
+/// @return the truth value;(-1 if the element's value is out of range);
+// int parse(char *arr, int floors, int current_floor)
+// {
+  
+// }
 
 
 //---------------------------------------------------------------------------------------------------------------------
